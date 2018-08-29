8212671372537ce17cd4c3cab99579479f1aa31f
Edson Nascimento <edson27bn@gmail.com>
2018-08-29 00:32:42 -0300

    Novos testes

--- test/ParcelamentoTest.php
+++ test/ParcelamentoTest.php
@@ -6,8 +6,17 @@ use Parcel\Parcelamento;
 class ParcelamentoTest {
     
     public static function runTest(){
-        $parcelamento = new Parcelamento(12, 6, 10.00, 0);
+        $parcelamento = new Parcelamento(12, 6, 10.00, 2.75);
         
         d($parcelamento->gerarParcelas('5587.56'));
+        
+        $parcelamento = new Parcelamento();
+        
+        $parcelamento->setMaxParcelas(10);
+        $parcelamento->setParcelasSemJuros(5);
+        $parcelamento->setValorMinimoParcelar(50.00);
+        $parcelamento->setPorcentagemJuros(2.50);
+        
+        d($parcelamento->gerarParcelas('777.77'));
     }
 }
